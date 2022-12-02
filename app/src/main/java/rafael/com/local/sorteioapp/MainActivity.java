5b3f669dc5a2e6f19fcbf1aea81c5f2a4621abcc
Rafael Antunes <rafao.lima@hotmail.com>
2022-12-02 10:39:55 -0300

    Initial commit

---
+++ app/src/main/java/rafael/com/local/sorteioapp/MainActivity.java
@@ -0,0 +1,29 @@
+package rafael.com.local.sorteioapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortear(View view){
+        //int Random = new Random().nextInt();
+        TextView texto = findViewById(R.id.resultado);
+
+        int num = new Random().nextInt(11);
+
+        texto.setText(String.format("O número sorteado foi: " + num));
+
+    }
+
+}
